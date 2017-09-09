252b7629d43e63535db8b0c7835210deff77b813
Blake Preston <blake.preston@hotmail.com>
2017-09-09 21:09:35 +1000

    Perfect forwarding in Reconstruct.

--- BlakesEngine/Core/beReconstruct.h
+++ BlakesEngine/Core/beReconstruct.h
@@ -1,11 +1,11 @@
 #pragma once
 
 template <typename T, typename ...Args>
-void Reconstruct(T* self, Args... args)
+void Reconstruct(T* self, Args&&... args)
 {
 	if (!std::is_trivially_destructible_v<T>)
 	{
 		self->~T();
 	}
-	new(self) T(args...);
+	new(self) T(std::forward<Args>(args)...);
 }
