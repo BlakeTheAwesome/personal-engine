e7076472c2714658184f6261f62549e8044cafcf
Blake Preston <blake.preston@hotmail.com>
2018-05-13 18:35:00 +1000

    Missing intersection files.

---
+++ BlakesEngine/Math/beIntersection.cpp
@@ -0,0 +1,66 @@
+#include "BlakesEngine/bePCH.h"
+#include "beIntersection.h"
+
+int beIntersection::TRayVsAABB(const Vec3& rayStart, const Vec3& rayDir, const Vec3& boxMin, const Vec3& boxMax, float collisions[2])
+{
+	float maxS = -FLT_MAX;
+	float minT = FLT_MAX;
+
+	for (int i : RangeIter(3))
+	{
+		float dir1 = VecElem(rayDir, i);
+		float start1 = VecElem(rayStart, i);
+		float boxMin1 = VecElem(boxMin, i);
+		float boxMax1 = VecElem(boxMax, i);
+
+		if (std::fabs(dir1) < 0.0001f)
+		{
+			if (start1 < boxMin1 || start1 > boxMax1)
+			{
+				return 0;
+			}
+		}
+		else
+		{
+			float invDir = 1.f/dir1;
+			float s = (boxMin1 - start1) * invDir;
+			float t = (boxMax1 - start1) * invDir;
+			if (s > t)
+			{
+				std::swap(s, t);
+			}
+			maxS = std::max(maxS, s);
+			minT = std::min(minT, t);
+
+			if (minT < 0.f || maxS > minT)
+			{
+				return 0;
+			}
+		}
+	}
+
+	collisions[0] = maxS;
+	collisions[1] = minT;
+	return 2;
+}
+
+std::optional<std::pair<float, float>> beIntersection::TRayVsAABB(const Vec3& rayStart, const Vec3& rayDir, const Vec3& boxMin, const Vec3& boxMax)
+{
+	float collisions[2];
+	if (TRayVsAABB(rayStart, rayDir, boxMin, boxMax, collisions) > 0)
+	{
+		return std::pair<float, float>{collisions[0], collisions[1]};
+	}
+	return {};
+
+}
+
+
+std::optional<beMath::Vec3> beIntersection::RayVsAABB(const Vec3& rayStart, const Vec3& rayDir, const Vec3& boxMin, const Vec3& boxMax)
+{
+	if (auto collisions = TRayVsAABB(rayStart, rayDir, boxMin, boxMax))
+	{
+		return rayStart + (rayDir * collisions->first);
+	}
+	return {};
+}
---
+++ BlakesEngine/Math/beIntersection.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "beMath.h"
+#include <optional>
+#include <tuple>
+
+namespace beIntersection
+{
+	// Collisions will be 0 at rayStart, 1 at rayStart+rayDir, negative if behind, >1 if past rayStart+rayDir
+	int TRayVsAABB(const Vec3& rayStart, const Vec3& rayDir, const Vec3& boxMin, const Vec3& boxMax, float collisions[2]);
+	std::optional<std::pair<float, float>> TRayVsAABB(const Vec3& rayStart, const Vec3& rayDir, const Vec3& boxMin, const Vec3& boxMax);
+
+	// returns first collision from rayStart in rayDir
+	std::optional<Vec3> RayVsAABB(const Vec3& rayStart, const Vec3& rayDir, const Vec3& boxMin, const Vec3& boxMax);
+}
