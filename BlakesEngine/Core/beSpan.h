aeacd2c2d26d70cea81fae15c1b224fc2ad11214
Blake Preston <blake.preston@hotmail.com>
2020-06-14 13:19:59 +1000

    Update gsl::span, to use until std::span is complete

--- BlakesEngine/Core/beSpan.h
+++ BlakesEngine/Core/beSpan.h
@@ -1,5 +1,28 @@
 #pragma once
+
+#if 0
 #include <span>
+#else
+#include "BlakesEngine/External/gsl/span"
+namespace std
+{
+	template <class ElementType, std::size_t Extent=(size_t)-1>
+	class span : public gsl::span<ElementType, Extent>
+	{
+		using gsl::span<ElementType, Extent>::span;
+	};
+
+	template <class Type, std::size_t Extent>
+	span(Type(&)[Extent])->span<Type, Extent>;
+
+	template <class Type, std::size_t Size>
+	span(std::array<Type, Size>&)->span<Type, Size>;
+
+	template <class Type, std::size_t Size>
+	span(const std::array<Type, Size>&)->span<const Type, Size>;
+}
+
+#endif
 
 #include "beConcepts.h"
 
